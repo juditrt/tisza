b5d446e28323168bbe7b44aa0a649c4ad67237b7
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2021-01-19 13:24:36 +0100

    test: integration test setup

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,5 +1,3 @@
-
-
 import MockDate from 'mockdate'
 import createInvoice from '../src/invoice/create'
 import createClient from '../src/szamlazzhu/create-client'
@@ -22,10 +20,7 @@ describe('integration', () => {
   test('simple', async () => {
     const invoice = await createInvoice(simpleOrder, simpleConfig);
     const client = createClient(simpleConfig, token)
-
     const xml = client._generateInvoiceXML(invoice)
-
     expect(xml).toMatchSnapshot();
   })
 });
-
